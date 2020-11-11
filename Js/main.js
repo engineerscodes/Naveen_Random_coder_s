e58f869a6dc5c567157beba6b1097b37bdb0909a
engineerscodes <practicalengineers05@gmail.com>
2020-11-12 01:33:31 +0530

    minor changes

---
+++ Js/main.js
@@ -0,0 +1,22 @@
+//Sticky nav menu /bar
+$(document).ready(function(){
+
+  let nav_offset_top=$('.header_rc').height()+50; 
+  function navbarfixed()
+  {
+      if($('.header_rc').length){
+          $(window).scroll(function(){
+            let scroll =$(window).scrollTop();
+            if(scroll>=nav_offset_top){
+                $(".header_rc .main_menu").addClass("navbar_fixed");
+               
+            }else{
+                $(".header_rc .main_menu").removeClass("navbar_fixed");
+            }
+          }) ;
+      }
+  }
+
+  navbarfixed();
+});
+
