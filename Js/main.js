187ec989ced5bd279d45fdf5776ea22131344e74
engineerscodes <practicalengineers05@gmail.com>
2020-11-25 20:32:32 +0530

    Mouse Animation

--- Js/main.js
+++ Js/main.js
@@ -20,3 +20,25 @@ $(document).ready(function(){
   navbarfixed();
 });
 
+
+/*Mouse Animation  */
+
+let mouseCursor=document.querySelector(".cursor");
+let navLinks=document.querySelectorAll(".navbar-nav li ");
+
+window.addEventListener('mousemove',cursor);
+
+function cursor( e){
+    mouseCursor.style.top=e.pageY+"px";
+    mouseCursor.style.left=e.pageX+'px';
+}
+
+navLinks.forEach(link =>{
+   link.addEventListener('mouseover',()=>{
+     mouseCursor.classList.add("link-grow");
+   });
+   link.addEventListener('mouseleave',()=>{
+    mouseCursor.classList.remove("link-grow");
+
+  });
+});
